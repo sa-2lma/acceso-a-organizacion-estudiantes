c9551c779d99333ab0fd60d84fbf689090481485
sa-2lma <sz855@inlumine.ual.es>
2026-05-06 15:39:12 +0200

    Completa ejercicios PR1 de Python

---
+++ PR1/aceleracionPR.py
@@ -0,0 +1,4 @@
+omega = float(input())
+radio = float(input())
+aceleracion = omega ** 2 * radio
+print(aceleracion)
---
+++ PR1/enordenPR.py
@@ -0,0 +1,5 @@
+numero_1 = float(input())
+numero_2 = float(input())
+numero_3 = float(input())
+
+print(numero_1 < numero_2 < numero_3)
